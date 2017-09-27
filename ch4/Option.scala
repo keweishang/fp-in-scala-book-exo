1963d44503880bd89f70153350054027234630ae
Kewei Shang <kewei.shang@gmail.com>
2017-09-27 21:52:23 +0200

    add Option

--- ch4/Option.scala
+++ ch4/Option.scala
@@ -3,6 +3,17 @@ sealed trait Option[+A] {
 		case None => None
 		case Some(a) => Some(f(a))
 	}
+
+	def flatMap[B](f: A => Option[B]): Option[B] = 
+		map(f) getOrElse None
+
+	def getOrElse[B >: A](default: => B): B = this match {
+		case None => default
+		case Some(a) => a
+	}
+
+	def orElse[B >: A](ob: => Option[B]): Option[B] = 
+		map(Some(_)) getOrElse ob
 }
 
 case class Some[+A](get: A) extends Option[A]
