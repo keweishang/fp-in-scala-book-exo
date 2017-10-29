1a68e5445d8096f701eb5fe4b608cb0c454ad7e9
Kewei Shang <kewei.shang@gmail.com>
2017-10-29 06:12:19 +0000

    add laziness example: Stream

---
+++ ch5/Stream.scala
@@ -0,0 +1,57 @@
+trait Stream[+A] {
+	// The natural recursive solution
+	def toListRecursive: List[A] = this match {
+		case Cons(h, t) => h() :: t().toListRecursive
+		case _ => List()
+	}
+
+	/*
+	The above solution will stack overflow for large streams, since it's
+	not tail-recursive. Here is a tail-recursive implementation. At each
+	step we cons onto the front of the `acc` list, which will result in the
+	reverse of the stream. Then at the end we reverse the result to get the
+	correct order again.
+	*/
+	def toList: List[A] = {
+		@annotation.tailrec
+		def go(s: Stream[A], acc: List[A]): List[A] = s match {
+			case Cons(h, t) => go(t(), h() :: acc)
+			case _ => acc
+		}
+		go(this, List()).reverse
+	}
+
+	/* 
+	In order to avoid the `reverse` at the end, we could write it using a
+	mutable list buffer and an explicit loop instead. Note that the mutable
+	list buffer never escapes our `toList` method, so this function is
+	still _pure_.
+	*/
+	def toListFast: List[A] = {
+		val buf = new collection.mutable.ListBuffer[A]
+		@annotation.tailrec
+		def go(s: Stream[A]): List[A] = s match {
+			case Cons(h, t) =>
+				buf += h()
+				go(t())
+			case _ => buf.toList
+		}
+		go(this)
+	}
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+	def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+		lazy val head = hd
+		lazy val tail = tl
+		Cons(() => head, () => tail)
+	}
+
+	def empty[A]: Stream[A] = Empty
+
+	def apply[A](as: A*): Stream[A] = 
+		if (as.isEmpty) Empty
+		else cons(as.head, apply(as.tail: _*))
+}
