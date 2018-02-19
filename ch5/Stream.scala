b16016aafe914b6ccf2397fe34534b761b09034f
Kewei Shang <kewei.shang@tapad.com>
2018-02-19 17:19:05 +0800

    update

--- ch5/Stream.scala
+++ ch5/Stream.scala
@@ -1,68 +1,79 @@
 trait Stream[+A] {
-	// The natural recursive solution
-	def toListRecursive: List[A] = this match {
-		case Cons(h, t) => h() :: t().toListRecursive
-		case _ => List()
-	}
+  import Stream._
+  // The natural recursive solution
+  def toListRecursive: List[A] = this match {
+    case Cons(h, t) => h() :: t().toListRecursive
+    case _ => List()
+  }
 
-	/*
-	The above solution will stack overflow for large streams, since it's
-	not tail-recursive. Here is a tail-recursive implementation. At each
-	step we cons onto the front of the `acc` list, which will result in the
-	reverse of the stream. Then at the end we reverse the result to get the
-	correct order again.
-	*/
-	def toList: List[A] = {
-		@annotation.tailrec
-		def go(s: Stream[A], acc: List[A]): List[A] = s match {
-			case Cons(h, t) => go(t(), h() :: acc)
-			case _ => acc
-		}
-		go(this, List()).reverse
-	}
+  /*
+  The above solution will stack overflow for large streams, since it's
+  not tail-recursive. Here is a tail-recursive implementation. At each
+  step we cons onto the front of the `acc` list, which will result in the
+  reverse of the stream. Then at the end we reverse the result to get the
+  correct order again.
+  */
+  def toList: List[A] = {
+    @annotation.tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h, t) => go(t(), h() :: acc)
+      case _ => acc
+    }
+    go(this, List()).reverse
+  }
 
-	/* 
-	In order to avoid the `reverse` at the end, we could write it using a
-	mutable list buffer and an explicit loop instead. Note that the mutable
-	list buffer never escapes our `toList` method, so this function is
-	still _pure_.
-	*/
-	def toListFast: List[A] = {
-		val buf = new collection.mutable.ListBuffer[A]
-		@annotation.tailrec
-		def go(s: Stream[A]): List[A] = s match {
-			case Cons(h, t) =>
-				buf += h()
-				go(t())
-			case _ => buf.toList
-		}
-		go(this)
-	}
+  /*
+  In order to avoid the `reverse` at the end, we could write it using a
+  mutable list buffer and an explicit loop instead. Note that the mutable
+  list buffer never escapes our `toList` method, so this function is
+  still _pure_.
+  */
+  def toListFast: List[A] = {
+    val buf = new collection.mutable.ListBuffer[A]
+    @annotation.tailrec
+    def go(s: Stream[A]): List[A] = s match {
+      case Cons(h, t) =>
+        buf += h()
+        go(t())
+      case _ => buf.toList
+    }
+    go(this)
+  }
 
-	/*
-	`take` first checks if n==0. In that case we need not look at the stream at all.
-	*/
-	def take(n: Int): Stream[A] = this match {
-		case Cons(h, t) => if n > 1 cons(h(), t().take(n-1))
-		case Cons(h, _) => if n == 1 cons(h(), empty)
-		case _ => empty
-	}
+  /*
+  `take` first checks if n==0. In that case we need not look at the stream at all.
+  */
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n-1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
 
-	
+  @annotation.tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n-1)
+    case _ => this
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => cons(h(), t() takeWhile p)
+    case _ => empty
+  }
+
 }
+
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
-
 object Stream {
-	def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
-		lazy val head = hd
-		lazy val tail = tl
-		Cons(() => head, () => tail)
-	}
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
 
-	def empty[A]: Stream[A] = Empty
+  def empty[A]: Stream[A] = Empty
 
-	def apply[A](as: A*): Stream[A] = 
-		if (as.isEmpty) Empty
-		else cons(as.head, apply(as.tail: _*))
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) Empty
+    else cons(as.head, apply(as.tail: _*))
 }
